4d2c49f365ab4e8578ee88e7209d73c00b1b4667
Abdurazzoq <abdurazzoqnegmatov31.03@gmail.com>
2025-12-22 19:23:48 +0500

    done motherfucker

--- src/pages/NavBar.vue
+++ src/pages/NavBar.vue
@@ -1,19 +1,50 @@
 <script>
-import {log10} from "chart.js/helpers";
+import lottie from 'lottie-web'
+import animationData from '@/assets/images/enter.json'
+import posts from '@/components/axios/posts';
 
 export default {
-  methods: {log10},
-  el: 'body',
+  methods: {
+    playOnce() {
+      if (!this.anim) return
+      this.anim.goToAndPlay(0, true)
+    },
+    openDoor(){
+      posts('https://api.mubingym.com/post/door')
+        .then((response) => {
+          console.log('Турникет открыт:', response.data);
+        })
+        .catch((error) => {
+          console.error('Ошибка при открытии турникета:', error);
+        });
+    }
+  },
   data() {
     return {
-      ActiveMenu: '/'
+      anim: null,
+      ActiveMenu: '/',
     }
   },
   watch: {
-    '$route' (to) {
+    '$route'(to) {
       this.ActiveMenu = to.fullPath;
+    },
+  },
+   mounted() {
+    this.anim = lottie.loadAnimation({
+      container: this.$refs.lottie,
+      renderer: 'svg',
+      loop: false,
+      autoplay: false,
+      animationData
+    })
+  },
+  beforeUnmount() {
+    if (this.anim) {
+      this.anim.destroy()
+      this.anim = null
     }
-  }
+  },
 }
 </script>
 
@@ -22,56 +53,86 @@ export default {
     <div class="row">
       <div class="col">
         <header class="header d-flex justify-content-between align-items-center">
-          <div class="logo-img"><img src="@/assets/images/logo%20gum.png"></div>
-          <div class="menu">
-            <ul class="d-flex justify-content-between align-items-center ">
-              <li :class="{active: ActiveMenu === '/'}"><router-link to="/" >Главная</router-link></li>
-              <li :class="{active: ActiveMenu === '/clients'|| ActiveMenu==='/user-page'}"><router-link to="/clients">Клиенты</router-link></li>
-              <li :class="{active: ActiveMenu === '/personal' || ActiveMenu === '/trainer-page'}"><router-link to="/personal">Персонал</router-link></li>
-              <li :class="{active: ActiveMenu === '/services'}"><router-link to="/services">Услуги</router-link></li>
-              <li :class="{active: ActiveMenu === '/courses'}">
-                <router-link to="/courses" class="trigger-hover">Курсы</router-link>
-                <!-- <ul class="hidden-hover bg-gray p-0 position-absolute mt-0 pt-4 rounded-3">
-                  <li :class="{active: ActiveMenu === '/individual-courses'}"><router-link to="/individual-courses" class="mx-3 my-2 p-3 mt-0">Курсы индивидуальные</router-link></li>
-                  <li :class="{active: ActiveMenu === '/reviews-page'}"><router-link to="/reviews-page" class="mx-3 my-2 p-3 mt-0">Курсы индивидуальные Отзывы</router-link></li>
-                </ul> -->
-              </li>
-              <li :class="{active: (ActiveMenu === '/finance' || ActiveMenu === '/transactions' || ActiveMenu === '/newsletters') }" class="trigger-hover">
+          <div class="logo-img"><img src="@/assets/images/logo%20gum.png" alt="Logo"></div>
+
+          <nav class="menu">
+            <ul class="d-flex justify-content-between align-items-center">
+              <li :class="{ active: ActiveMenu === '/' }"><router-link to="/">Главная</router-link></li>
+              <li :class="{ active: ActiveMenu === '/clients' || ActiveMenu === '/user-page' }"><router-link
+                  to="/clients">Клиенты</router-link></li>
+              <li :class="{ active: ActiveMenu === '/personal' || ActiveMenu === '/trainer-page' }"><router-link
+                  to="/personal">Персонал</router-link></li>
+              <li :class="{ active: ActiveMenu === '/services' }"><router-link to="/services">Услуги</router-link></li>
+              <li :class="{ active: ActiveMenu === '/courses' }"><router-link to="/courses">Курсы</router-link></li>
+
+              <li :class="{ active: ['/finance', '/transactions', '/newsletters'].includes(ActiveMenu) }"
+                class="trigger-hover">
                 <a>Управление</a>
                 <ul class="hidden-hover bg-gray p-0 position-absolute mt-0 pt-4 rounded-3">
-                  <li :class="{active: ActiveMenu === '/transactions'}"><router-link to="/transactions" class="mx-3 p-3 my-2 p-3 mt-0">Транзакции</router-link></li>
-                  <li :class="{active: ActiveMenu === '/finance'}"><router-link to="/finance" class="mx-3 p-3 my-2 p-3 mt-0">Финансы</router-link></li>
-                  <li :class="{active: ActiveMenu === '/newsletters'}"><router-link to="/newsletters" class="mx-3 p-3 my-2 p-3 mt-0">Рассылки</router-link></li>
+                  <li :class="{ active: ActiveMenu === '/transactions' }"><router-link
+                      to="/transactions">Транзакции</router-link></li>
+                  <li :class="{ active: ActiveMenu === '/finance' }"><router-link to="/finance">Финансы</router-link>
+                  </li>
+                  <li :class="{ active: ActiveMenu === '/newsletters' }"><router-link
+                      to="/newsletters">Рассылки</router-link></li>
                 </ul>
               </li>
-              <li :class="{active: ActiveMenu === '/lockers'}"><router-link to="/lockers">Шкафчики</router-link></li>
-              <!-- <li :class="{active: ActiveMenu === '/product'}"><router-link to="/product">Продукты</router-link></li> -->
-              <li :class="{active: ActiveMenu === '/warehouse' || ActiveMenu.includes('/warehouse/') || ActiveMenu.includes('/warehouseItem/') }"><router-link to="/warehouse">Склад</router-link></li>
-              <li :class="{active: ActiveMenu === '/cash'}"><router-link to="/cash">Касса</router-link></li>
+
+              <li :class="{ active: ActiveMenu === '/lockers' }"><router-link to="/lockers">Шкафчики</router-link></li>
+              <li :class="{ active: ActiveMenu.startsWith('/warehouse') }"><router-link
+                  to="/warehouse">Склад</router-link></li>
+              <li :class="{ active: ActiveMenu === '/cash' }"><router-link to="/cash">Касса</router-link></li>
             </ul>
-          </div>
-          <router-link to="/login"><div class="user-icon">
-            <img height="24" width="24" src="@/assets/images/icons/user-icon.png" alt="user" style="width: 100%;">
-          </div></router-link>
+          </nav>
+
+          <router-link to="/login">
+            <div class="user-icon">
+              <img height="24" width="24" src="@/assets/images/icons/user-icon.png" alt="Пользователь">
+            </div>
+          </router-link>
         </header>
       </div>
     </div>
   </div>
+
+  <button @click="playOnce(), openDoor()" class="turnstile-btn d-flex align-items-center gap-2 mt-3 bg-yellow rounded-3 px-3 py-2 position-absolute end-0 bottom-0 m-4">
+    <div ref="lottie" class="lottie-icon"></div>
+    Открыть турникет
+  </button>
+
+
 </template>
 
 <style scoped>
-/* Add your styles here */
-  .hidden-hover{
-    margin-top: 23px;
-    background: #2c2c2e;
-  }
+.trigger-hover {
+  position: relative;
+}
 
-@media screen and (max-width: 1290px){
-  .menu ul{
+.hidden-hover {
+  display: none;
+  margin-top: 23px;
+  background: #2c2c2e;
+  min-width: 200px;
+  z-index: 1000;
+}
+
+.trigger-hover:hover .hidden-hover {
+  display: block;
+}
+
+@media screen and (max-width: 1290px) {
+  .menu ul {
     gap: 18px;
   }
-  .menu ul li a{
+
+  .menu ul li a {
     font-size: 14px;
   }
 }
+
+.lottie-icon {
+  width: 40px;
+  height: 40px;
+}
+
 </style>
