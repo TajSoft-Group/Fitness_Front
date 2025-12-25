ce55abbd466943bfaca8d4a11da09c3f1c4d9393
Abdurazzoq <abdurazzoqnegmatov31.03@gmail.com>
2025-12-25 17:03:57 +0500

    turniket

--- src/pages/NavBar.vue
+++ src/pages/NavBar.vue
@@ -97,11 +97,11 @@ export default {
     </div>
   </div>
 
-  <!-- <button @click="playOnce(), openDoor()"
+  <button @click="playOnce(), openDoor()"
     class="turnstile-btn d-flex align-items-center gap-2 mt-3 bg-yellow rounded-3 px-3 py-2 position-fixed end-0 bottom-0 m-4">
     <div ref="lottie" class="lottie-icon"></div>
     Открыть турникет
-  </button> -->
+  </button>
 
 
 </template>
